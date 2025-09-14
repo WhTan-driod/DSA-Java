646a18cc40f61aaa5059052b644768c32a50f9ac
Henry <henry.tanguy@gmail.com>
2025-09-14 16:07:17 -0400

    added comments and demonstrated bracket matching.

--- HenryTanguyHw2Stacks/BracketMatching.java
+++ HenryTanguyHw2Stacks/BracketMatching.java
@@ -1,13 +1,24 @@
+//Class to determin if string has an equal number of opening and closing brackets
 public class BracketMatching {
     public static boolean isBalanced(String string){
+
+        //create new stack object from LinkedListStack.java file
         LinkedListStack<Character> stack = new LinkedListStack<>();
+
+        //for loop to iterate through string
         for(char ch : string.toCharArray()){
+
+            //add character to stack if opening bracket
             if(ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
+
+            //check it closing bracket
             } else if(ch == ')' || ch == '}' || ch == ']'){
                 if(stack.isEmpty()){
                     return false; // Unmatched closing bracket
                 }
+
+                //match closing bracket with appropriate closing bracket
                 char top = stack.pop();
                 if((ch == ')' && top != '(') ||
                    (ch == '}' && top != '{') ||
@@ -18,4 +29,13 @@ public class BracketMatching {
         }
         return stack.isEmpty(); // True if all opening brackets are matched
     }
+        public static void main(String[] args) {
+            String emptyString = "";
+            String balancedChars = "[{(((())))}]";
+            String unbalancedChars = "({({})";
+
+            System.out.println(BracketMatching.isBalanced(emptyString));
+            System.out.println(BracketMatching.isBalanced(balancedChars));
+            System.out.println(BracketMatching.isBalanced(unbalancedChars));
+        }
 }
--- HenryTanguyHw2Stacks/LinkedListStack.java
+++ HenryTanguyHw2Stacks/LinkedListStack.java
@@ -1,7 +1,9 @@
+//class to demonstrate a stack through a singly linked list
 public class LinkedListStack<T> {
     private Node<T> top;
     private int size;
 
+    //Node class to store data and point to next node
     private static class Node<T> {
         T data;
         Node<T> next;
@@ -11,11 +13,13 @@ public class LinkedListStack<T> {
         }
     }
 
+    //constructor
     public LinkedListStack() {
         this.top = null;
         this.size = 0;
     }
 
+    //push node onto stack
     public void push(T item) {
         Node<T> newNode = new Node<>(item);
         newNode.next = top;
@@ -23,6 +27,7 @@ public class LinkedListStack<T> {
         size++;
     }
 
+    //remove node from stack
     public T pop() {
         if (isEmpty()) {
             throw new IllegalStateException("Stack is empty");
@@ -33,6 +38,7 @@ public class LinkedListStack<T> {
         return item;
     }
 
+    //display top bode
     public T peek() {
         if (isEmpty()) {
             throw new IllegalStateException("Stack is empty");
@@ -40,21 +46,26 @@ public class LinkedListStack<T> {
         return top.data;
     }
 
+    //check if stack is empty
     public boolean isEmpty() {
         return size == 0;
     }
 
+    //check size of stack
     public int size() {
         return size;
     }
 
     public static void main(String[] args) {
+        //create new stack object
         LinkedListStack<Integer> stack = new LinkedListStack<>();
         
+        //add elements to stack
         stack.push(10);
         stack.push(20);
         stack.push(30);
         
+        //call stack methods
         System.out.println("Top element: " + stack.peek()); // 30
         System.out.println("Stack size: " + stack.size()); // 3
         
